b0ad44593d97054c8f6e79ca92ff01edec162eff
Jeffrey Kuna <jeffreykuna@gmail.com>
2014-07-26 18:20:27 -0700

    added simple nested files

--- pyJConf.py
+++ pyJConf.py
@@ -2,12 +2,13 @@
 
 # Jeff's Configuration Module
 #
-# JSON(ish) based configuration system that supports inline comments and easy
-# switching between profiles.
+# JSON(ish) based configuration system that supports inline comments, easy
+# switching between profiles, and nested files.
 #
 # Comments are started with // as the first non-whitespace characters on each
 # line. Profile specific lines are indicated with @profile as the first
-# non-whitespace characters on each line.
+# non-whitespace characters on each line. Nested files are included with
+# include(filename).
 
 import json
 
@@ -22,20 +23,29 @@ class JConf:
             raise JConfSyntaxError
 
 
-    def _parser(self, filename, profile):
+    def _parser(self, filename, profile, filelist = None):
         temp_data = []
+        if filelist:
+            if filename in filelist: raise JConfRecursiveFileError
+            filelist.append(filename)
+        else:
+            filelist = [filename]
         with open(filename) as f:
             for line in f:
                 linestripped = line.strip()
                 if linestripped.startswith('//'): continue
                 if linestripped.startswith('@'):
                     if linestripped.startswith('@' + profile):
-                        temp_data.append(linestripped[len(profile) + 1:])
-                else:
-                    temp_data.append(linestripped)
-        
+                        linestripped = linestripped[len(profile) + 1:].strip()
+                    else:
+                        continue
+                if linestripped.startswith('include('):
+                    linestripped = linestripped[len('include('):]
+                    subfilename = linestripped.split(')', 1)[0]
+                    linestripped = self._parser(subfilename, profile, filelist)
+                temp_data.append(linestripped)
         return ''.join(temp_data)
-
+        
 
     def __getattr__(self, name):
         if name in self._settings_dict:
@@ -49,4 +59,8 @@ class SettingUnspecifiedError(LookupError):
 
 
 class JConfSyntaxError(ValueError):
+    pass
+
+
+class JConfRecursiveFileError(Exception):
     pass
--- pyJConf_tests.py
+++ pyJConf_tests.py
@@ -5,7 +5,7 @@ import os
 import exceptions
 
 from pyJConf import JConf
-from pyJConf import SettingUnspecifiedError, JConfSyntaxError
+from pyJConf import SettingUnspecifiedError, JConfSyntaxError, JConfRecursiveFileError
 
 class TestSettingsLoad(unittest.TestCase):
     def setUp(self):
@@ -26,6 +26,59 @@ class TestSettingsLoad(unittest.TestCase):
 
         f = open("temp_file_two.jconf", 'w+')
         f.write(file_two)
+        f.close()
+
+        file_three = """{
+        "setting": "A",
+@user "usersetting": 5,
+include(temp_extra.jconf)
+@user include(user_extra.jconf)
+}"""
+
+        f = open("temp_file_three.jconf", 'w+')
+        f.write(file_three)
+        f.close()
+
+        file_four = """
+"extralist": [7, 8, 9]
+"""
+
+        f = open("temp_extra.jconf", 'w+')
+        f.write(file_four)
+        f.close()
+
+        file_five = """
+include(user_extra2.jconf)
+"""
+
+        f = open("user_extra.jconf", 'w+')
+        f.write(file_five)
+        f.close()
+
+        file_six = """
+,"usernested": "Hello"
+"""
+
+        f = open("user_extra2.jconf", 'w+')
+        f.write(file_six)
+        f.close()
+
+        file_seven = """
+{"A": 5,
+include(temp_recursiveB.jconf)
+}
+"""
+
+        f = open("temp_recursive.jconf", 'w+')
+        f.write(file_seven)
+        f.close()
+
+        file_eight = """
+include(temp_recursive.jconf)
+"""
+
+        f = open("temp_recursiveB.jconf", 'w+')
+        f.write(file_eight)
         f.close()
 
 
@@ -57,9 +110,33 @@ class TestSettingsLoad(unittest.TestCase):
             s = JConf("temp_file_two.jconf")
 
 
+    def test_nested(self):
+        s = JConf("temp_file_three.jconf")
+        self.assertTrue(s.setting == 'A')
+        self.assertTrue(s.extralist == [7, 8, 9])
+
+
+    def test_nestedinclude(self):
+        s = JConf("temp_file_three.jconf", "user")
+        self.assertTrue(s.setting == 'A')
+        self.assertTrue(s.extralist == [7, 8, 9])
+        self.assertTrue(s.usernested == "Hello")
+
+
+    def test_recursiveerror(self):
+        with self.assertRaises(JConfRecursiveFileError):
+            s = JConf("temp_recursive.jconf")
+
+
     def tearDown(self):
         os.remove("temp_file_one.jconf")
-
+        os.remove("temp_file_two.jconf")
+        os.remove("temp_file_three.jconf")
+        os.remove("temp_extra.jconf")
+        os.remove("user_extra.jconf")
+        os.remove("user_extra2.jconf")
+        os.remove("temp_recursive.jconf")
+        os.remove("temp_recursiveB.jconf")
 
 if __name__ == '__main__':
     unittest.main()
